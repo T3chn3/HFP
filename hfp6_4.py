5eed3461ee0d1e3f05b3e4743966b1e595bafdb3
T3chn3 <joseph.bihms@gmail.com>
2017-01-15 06:50:13 +0000

    Inheritance Example
    Created a subclass using a bif as the base

--- hfp6_4.py
+++ hfp6_4.py
@@ -1,8 +1,4 @@
-#This example reads in data from a file, creates a class, with extended methods to add times. 
-#The data is sorted and printed to screen
-
-#self refers to the current object instance
-
+#inheritance of the list bif, AthleteList can now use all list functions
 def sanitize(time_string):
     if '-' in time_string:
         splitter = '-'
@@ -13,27 +9,23 @@ def sanitize(time_string):
     (mins, secs) = time_string.split(splitter)
     return(mins + '.' + secs)
 
-class Athlete: #pretty much a wrapper for the list class
+class AthleteList(list):
+
     def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
         self.name = a_name
         self.dob = a_dob
-        self.times = a_times
-        
+        self.extend(a_times)
+
     def top3(self):
-        return(sorted(set([sanitize(t) for t in self.times]))[0:3])
-    
-    def add_time(self, time):
-        self.times.append(time)  
-        
-    def add_times(self, times):
-        self.times.extend(times)
+        return(sorted(set([sanitize(t) for t in self]))[0:3])
         
 def get_coach_data(filename):
     try:
         with open(filename) as f:
             data = f.readline()
         templ = data.strip().split(',')
-        return(Athlete(templ.pop(0), templ.pop(0), templ))
+        return(AthleteList(templ.pop(0), templ.pop(0), templ))
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return(None)
@@ -47,43 +39,3 @@ print(james.name + "'s fastest times are: " + str(james.top3()))
 print(julie.name + "'s fastest times are: " + str(julie.top3()))
 print(mikey.name + "'s fastest times are: " + str(mikey.top3()))
 print(sarah.name + "'s fastest times are: " + str(sarah.top3()))
-
-sarah.add_time('0.12')
-sarah.add_times(['1.99','2.66'])
-print(sarah.name + "'s fastest times are: " + str(sarah.top3()))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-sarah = get_coach_data('sarah2.txt')
